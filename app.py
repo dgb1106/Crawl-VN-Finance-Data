06f39a6913241fd47b889e061f6c81efdfe68e26
dgb1106 <duonggiabao1106@gmail.com>
2025-03-26 20:43:26 +0700

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 import requests
 import WebScraping
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/indices', methods=['GET'])
 def indices():
